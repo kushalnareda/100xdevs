c64e14680314e85ae7229a465596a01f3f0b07fe
Kushal Singh Nareda <128476254+kushalnareda@users.noreply.github.com>
2023-12-18 03:22:42 +0530

    Update 4-promise-chain.js

--- Assignments/Assignment 2/01-async-js/hard (promises)/4-promise-chain.js
+++ Assignments/Assignment 2/01-async-js/hard (promises)/4-promise-chain.js
@@ -4,21 +4,41 @@
  * Return a promise chain which return the time in milliseconds it takes to complete the entire operation.
  * Compare it with the results from 3-promise-all.js
  */
+function sleep(milliseconds) {
+    return new Promise((resolve, reject) => {
+        if (typeof milliseconds != 'number' || milliseconds<0){
+            reject(new Error('Undifined'));
+        }
+        if(milliseconds<=0)
+            resolve();
+        else{
+            const startTime = new Date().getTime();
+            while ((new Date().getTime() - startTime) < milliseconds) {
+                //let it wait 
+            }
+            resolve();
+        }
+    })
+}
 
-function wait1(t) {
+async function wait1(t) {
+    return sleep(t*1000);
+}
+
+async function  wait2(t) {
+    return sleep(t*1000);
 
 }
 
-function wait2(t) {
-
+async function  wait3(t) {
+    return sleep(t*1000);
 }
 
-function wait3(t) {
-
-}
-
-function calculateTime(t1, t2, t3) {
-
+async function calculateTime(t1, t2, t3) {
+    const startTime = (new Date()).getTime();
+    await  Promise.all([wait1(t1),wait2(t2),wait3(t3)]);
+    const endTime = (new Date()).getTime();
+    return endTime - startTime;
 }
 
 module.exports = calculateTime;
